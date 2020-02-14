43ca444cddce656c906cfa0cd9a02e2db5ee0646
Dionatan <dionatan.medeiros@triersistemas.com.br>
2020-02-14 13:16:11 -0300

    feat: Created about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Container } from "./styles"
+
+export default function About() {
+  return (
+    <Container>
+      <h1>About Page</h1>
+    </Container>
+  )
+}
