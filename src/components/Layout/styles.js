c5878014ff60d7f1cb63c332f71e9d1ca6dd4c03
DiouMedeiros <diou.182@gmail.com>
2020-03-04 23:16:14 -0300

    chore: Default layout adjusted

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  min-height: 93.2vh;
-  background: #282a36;
+  min-height: 100vh;
+  background: #303846;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,13 +12,13 @@ export const Container = styled.div`
     text-align: center;
 
     img {
-      border: 4px solid #4ef579;
+      border: 4px solid #09d3ac;
       border-radius: 50%;
       margin: 50px 0;
     }
 
     strong {
-      color: #4ef579;
+      color: #09d3ac;
       font-size: 32px;
     }
 
@@ -29,6 +29,7 @@ export const Container = styled.div`
   }
 
   footer {
+    height: 50px;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -36,7 +37,10 @@ export const Container = styled.div`
 
     svg {
       &:hover {
-        color: #9e9e9e !important;
+        color: #09d3ac !important;
+        height: 40px;
+        width: 40px;
+        transition: 0.3s;
       }
     }
   }
